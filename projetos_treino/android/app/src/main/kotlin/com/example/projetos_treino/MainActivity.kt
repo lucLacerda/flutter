efa045d3634a9f32332ce36ded5858eeba9cd47a
Lucas Lacerda <89214092+Lucaszib@users.noreply.github.com>
2022-03-27 02:48:11 -0300

    organize folders

--- projetos_treino/android/app/src/main/kotlin/com/example/projetos_treino/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.projetos_treino
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- projetos_treino/tarefas_lucas/android/app/src/main/kotlin/com/example/tarefas_lucas/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tarefas_lucas
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
